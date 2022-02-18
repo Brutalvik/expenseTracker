3f63b681df1898c1461f9f21b353a5d3c079f1a1
Brutalvik <66142815+Brutalvik@users.noreply.github.com>
2022-02-18 01:53:42 -0500

    Changes to Expense Form Component

--- src/components/AddExpense/ExpenseForm.js
+++ src/components/AddExpense/ExpenseForm.js
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import Form from "../UI/Form";
 import Button from "../UI/Button";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  const titleHandler = (e) => {
+    setEnteredTitle(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(enteredTitle);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="add-expense__controls">
-        <Form type="text" title="Title" />
+        <Form type="text" title="Title" onchange={titleHandler} />
         <Form type="number" title="Amount" min="0.01" step="0.01" />
         <Form type="date" title="Date" min="2019-01-01" max="2024-12-31" />
       </div>
--- src/components/UI/Form.js
+++ src/components/UI/Form.js
@@ -1,10 +1,10 @@
 import "./Form.css";
 
-const ExpenseForm = ({ title, type, min, max, step }) => {
+const ExpenseForm = ({ title, type, min, max, step, onchange }) => {
   return (
     <div className="add-expense__control">
       <label>{title}</label>
-      <input type={type} min={min} max={max} step={step} />
+      <input type={type} min={min} max={max} step={step} onChange={onchange} />
     </div>
   );
 };
