bcc54a3dc0f089f803f5a1ebb1077295dbd1f066
Brutalvik <66142815+Brutalvik@users.noreply.github.com>
2022-02-18 02:09:16 -0500

    changed the user input to single state

--- src/components/AddExpense/ExpenseForm.js
+++ src/components/AddExpense/ExpenseForm.js
@@ -4,23 +4,56 @@ import Button from "../UI/Button";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState("");
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
 
   const titleHandler = (e) => {
-    setEnteredTitle(e.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: e.target.value,
+    });
+  };
+
+  const amountHandler = (e) => {
+    setUserInput({
+      ...userInput,
+      enteredAmount: e.target.value,
+    });
+  };
+
+  const dateHandler = (e) => {
+    setUserInput({
+      ...userInput,
+      enteredDate: e.target.value,
+    });
   };
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(enteredTitle);
+    console.log(userInput);
   };
 
   return (
     <form onSubmit={submitHandler}>
       <div className="add-expense__controls">
         <Form type="text" title="Title" onchange={titleHandler} />
-        <Form type="number" title="Amount" min="0.01" step="0.01" />
-        <Form type="date" title="Date" min="2019-01-01" max="2024-12-31" />
+        <Form
+          type="number"
+          title="Amount"
+          min="0.01"
+          step="0.01"
+          onchange={amountHandler}
+        />
+        <Form
+          type="date"
+          title="Date"
+          min="2019-01-01"
+          max="2024-12-31"
+          onchange={dateHandler}
+        />
       </div>
       <div className="add-expense__actions">
         <Button type="submit" text="Add Expense" />
